a40e51dece63bce61cb54615b48f8b73e6e59751
Sweetysweet <gsm_v@mail.ru>
2022-05-12 12:44:48 +0300

    add detail

--- components/Details.vue
+++ components/Details.vue
@@ -1,23 +1,40 @@
 <template>
   <section class="details">
     <div class="details__container">
-      <div class="image-block__image">
-        <span>Картинка</span>
-      </div>
       <div class="imagine-block__text">
-        <div class="imagine-block__text--title">
-          <span class="imagine-block__text--title">Imagine yourself as a character</span>
+        <div class="imagine-block__text-inner">
+          <span class="imagine-block__text-inner-part imagine-block__text-inner-part--title">Imagine yourself</span>
+          <span class="imagine-block__text-inner-part imagine-block__text-inner-part--subtitle">as a character</span>
         </div>
-        <div class="imagine-block__text--description">
-          <span>
+        <div class="imagine-block__text-description">
+          <span class="imagine-block__text-description-inner imagine-block__text-description-inner--light">
             Pick a character considering all your anatomical characteristics and
             preferences. Successful?</span
           >
-          <span
-            >You can order your kit from the Marketplace right away. Created a
+          <p class="imagine-block__text-description-inner imagine-block__text-description-inner--bold"
+            >You can order your kit from the Marketplace right away.
+          <span class="imagine-block__text-description imagine-block__text-description-inner--light">Created a
             unique character? Order the production of the costume and
-            accessories.</span
+            accessories.</span>
+          </p>
+        </div>
+      </div>
+      <div class="imagine-block__text">
+        <div class="imagine-block__text-inner">
+          <span class="imagine-block__text-inner-part imagine-block__text-inner-part--title">Imagine yourself</span>
+          <span class="imagine-block__text-inner-part imagine-block__text-inner-part--subtitle">as a character</span>
+        </div>
+        <div class="imagine-block__text-description">
+          <span class="imagine-block__text-description-inner imagine-block__text-description-inner--light">
+            Pick a character considering all your anatomical characteristics and
+            preferences. Successful?</span
           >
+          <p class="imagine-block__text-description-inner imagine-block__text-description-inner--bold"
+            >You can order your kit from the Marketplace right away.
+          <span class="imagine-block__text-description imagine-block__text-description-inner--light">Created a
+            unique character? Order the production of the costume and
+            accessories.</span>
+          </p>
         </div>
       </div>
     </div>
--- components/Hero.vue
+++ components/Hero.vue
@@ -1,148 +1,156 @@
 <template>
   <section class="hero">
-      <div class="hero__container">
-        <div class="hero-top">
-          <span>Life</span>
-        </div>
-        <div class="hero-title">
-          <span class="hero-title__large">INCOS</span>
-          <span class="hero-title__large">PLAY</span>
-        </div>
-        <div class="hero-subtitle">
-          <span>Social network</span>
-        </div>
-        <div class="hero-download">
-          <a href="https://apps.apple.com/app/id1473016754" target="blank" class="hero-download__link hero-download__link--left">
-            <svg
-              width="120"
-              height="120"
-              viewBox="0 0 120 120"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g filter="url(#filter0_d_1_158)">
-                <path
-                  d="M55.9879 39.6573L57.0887 37.7581C57.7661 36.5726 59.2782 36.1734 60.4637 36.8508C61.6492 37.5282 62.0484 39.0403 61.371 40.2258L50.7863 58.5524H58.4436C60.9234 58.5524 62.3145 61.4677 61.2379 63.4879H38.7984C37.4315 63.4879 36.3306 62.3871 36.3306 61.0202C36.3306 59.6532 37.4315 58.5524 38.7984 58.5524H45.0887L53.1452 44.5927L50.629 40.2258C49.9516 39.0403 50.3508 37.5403 51.5363 36.8508C52.7218 36.1734 54.2218 36.5726 54.9113 37.7581L55.9879 39.6573ZM46.4677 66.0282L44.0968 70.1411C43.4194 71.3266 41.9073 71.7258 40.7218 71.0484C39.5363 70.371 39.1371 68.8589 39.8145 67.6734L41.5806 64.625C43.5645 64.0081 45.1855 64.4798 46.4677 66.0282ZM66.8992 58.5645H73.3226C74.6895 58.5645 75.7903 59.6653 75.7903 61.0323C75.7903 62.3992 74.6895 63.5 73.3226 63.5H69.754L72.1613 67.6734C72.8387 68.8589 72.4395 70.3589 71.254 71.0484C70.0685 71.7258 68.5686 71.3266 67.879 70.1411C63.8266 63.1129 60.7782 57.8508 58.7581 54.3427C56.6895 50.7742 58.1653 47.1935 59.629 45.9839C61.25 48.7661 63.6694 52.9637 66.8992 58.5645ZM56 26C39.4274 26 26 39.4274 26 56C26 72.5726 39.4274 86 56 86C72.5726 86 86 72.5726 86 56C86 39.4274 72.5726 26 56 26ZM82.129 56C82.129 70.3589 70.504 82.129 56 82.129C41.6411 82.129 29.871 70.504 29.871 56C29.871 41.6411 41.496 29.871 56 29.871C70.3589 29.871 82.129 41.496 82.129 56Z"
-                  fill="url(#paint0_linear_1_158)"
+    <div class="hero__container">
+      <div class="hero-top">
+        <span>Life</span>
+      </div>
+      <div class="hero-title">
+        <span class="hero-title__large">INCOS</span>
+        <span class="hero-title__large">PLAY</span>
+      </div>
+      <div class="hero-subtitle">
+        <span>social network</span>
+      </div>
+      <div class="hero-download">
+        <a
+          href="https://apps.apple.com/app/id1473016754"
+          target="blank"
+          class="hero-download__link hero-download__link--left"
+        >
+          <svg
+            width="120"
+            height="120"
+            viewBox="0 0 120 120"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g filter="url(#filter0_d_1_158)">
+              <path
+                d="M55.9879 39.6573L57.0887 37.7581C57.7661 36.5726 59.2782 36.1734 60.4637 36.8508C61.6492 37.5282 62.0484 39.0403 61.371 40.2258L50.7863 58.5524H58.4436C60.9234 58.5524 62.3145 61.4677 61.2379 63.4879H38.7984C37.4315 63.4879 36.3306 62.3871 36.3306 61.0202C36.3306 59.6532 37.4315 58.5524 38.7984 58.5524H45.0887L53.1452 44.5927L50.629 40.2258C49.9516 39.0403 50.3508 37.5403 51.5363 36.8508C52.7218 36.1734 54.2218 36.5726 54.9113 37.7581L55.9879 39.6573ZM46.4677 66.0282L44.0968 70.1411C43.4194 71.3266 41.9073 71.7258 40.7218 71.0484C39.5363 70.371 39.1371 68.8589 39.8145 67.6734L41.5806 64.625C43.5645 64.0081 45.1855 64.4798 46.4677 66.0282ZM66.8992 58.5645H73.3226C74.6895 58.5645 75.7903 59.6653 75.7903 61.0323C75.7903 62.3992 74.6895 63.5 73.3226 63.5H69.754L72.1613 67.6734C72.8387 68.8589 72.4395 70.3589 71.254 71.0484C70.0685 71.7258 68.5686 71.3266 67.879 70.1411C63.8266 63.1129 60.7782 57.8508 58.7581 54.3427C56.6895 50.7742 58.1653 47.1935 59.629 45.9839C61.25 48.7661 63.6694 52.9637 66.8992 58.5645ZM56 26C39.4274 26 26 39.4274 26 56C26 72.5726 39.4274 86 56 86C72.5726 86 86 72.5726 86 56C86 39.4274 72.5726 26 56 26ZM82.129 56C82.129 70.3589 70.504 82.129 56 82.129C41.6411 82.129 29.871 70.504 29.871 56C29.871 41.6411 41.496 29.871 56 29.871C70.3589 29.871 82.129 41.496 82.129 56Z"
+                fill="url(#paint0_linear_1_158)"
+              />
+            </g>
+            <defs>
+              <filter
+                id="filter0_d_1_158"
+                x="0"
+                y="0"
+                width="120"
+                height="120"
+                filterUnits="userSpaceOnUse"
+                color-interpolation-filters="sRGB"
+              >
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feColorMatrix
+                  in="SourceAlpha"
+                  type="matrix"
+                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                  result="hardAlpha"
                 />
-              </g>
-              <defs>
-                <filter
-                  id="filter0_d_1_158"
-                  x="0"
-                  y="0"
-                  width="120"
-                  height="120"
-                  filterUnits="userSpaceOnUse"
-                  color-interpolation-filters="sRGB"
-                >
-                  <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                  <feColorMatrix
-                    in="SourceAlpha"
-                    type="matrix"
-                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                    result="hardAlpha"
-                  />
-                  <feOffset dx="4" dy="4" />
-                  <feGaussianBlur stdDeviation="15" />
-                  <feComposite in2="hardAlpha" operator="out" />
-                  <feColorMatrix
-                    type="matrix"
-                    values="0 0 0 0 0.741176 0 0 0 0 0 0 0 0 0 1 0 0 0 0.25 0"
-                  />
-                  <feBlend
-                    mode="normal"
-                    in2="BackgroundImageFix"
-                    result="effect1_dropShadow_1_158"
-                  />
-                  <feBlend
-                    mode="normal"
-                    in="SourceGraphic"
-                    in2="effect1_dropShadow_1_158"
-                    result="shape"
-                  />
-                </filter>
-                <linearGradient
-                  id="paint0_linear_1_158"
-                  x1="66.8994"
-                  y1="-3.43391"
-                  x2="130.149"
-                  y2="46.8165"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop offset="0.286458" stop-color="#93EFD9" />
-                  <stop offset="1" stop-color="#ED93EF" />
-                  <stop offset="1" stop-color="#ED93EF" />
-                </linearGradient>
-              </defs>
-            </svg>
-          </a>
-          <a href="https://apps.apple.com/app/id1473016754" target="blank" class="hero-download__link hero-download__link--right">
-            <svg
-              width="110"
-              height="120"
-              viewBox="0 0 110 120"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g filter="url(#filter0_d_1_156)">
-                <path
-                  d="M58.506 53.4624L34.5924 27.5237L65.018 46.4181L58.506 53.4624ZM28.3513 26C26.9427 26.797 26 28.2504 26 30.1375V81.8625C26 83.7496 26.9427 85.203 28.3513 86L56.1547 55.9941L28.3513 26ZM74.4231 52.4427L68.0411 48.4458L60.9223 56.0059L68.0411 63.5659L74.5531 59.5691C76.5034 57.8929 76.5034 54.1188 74.4231 52.4427ZM34.5924 84.488L65.018 65.5937L58.506 58.5493L34.5924 84.488Z"
-                  fill="url(#paint0_linear_1_156)"
+                <feOffset dx="4" dy="4" />
+                <feGaussianBlur stdDeviation="15" />
+                <feComposite in2="hardAlpha" operator="out" />
+                <feColorMatrix
+                  type="matrix"
+                  values="0 0 0 0 0.741176 0 0 0 0 0 0 0 0 0 1 0 0 0 0.25 0"
                 />
-              </g>
-              <defs>
-                <filter
-                  id="filter0_d_1_156"
-                  x="0"
-                  y="0"
-                  width="110"
-                  height="120"
-                  filterUnits="userSpaceOnUse"
-                  color-interpolation-filters="sRGB"
-                >
-                  <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                  <feColorMatrix
-                    in="SourceAlpha"
-                    type="matrix"
-                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                    result="hardAlpha"
-                  />
-                  <feOffset dx="4" dy="4" />
-                  <feGaussianBlur stdDeviation="15" />
-                  <feComposite in2="hardAlpha" operator="out" />
-                  <feColorMatrix
-                    type="matrix"
-                    values="0 0 0 0 0.741176 0 0 0 0 0 0 0 0 0 1 0 0 0 0.25 0"
-                  />
-                  <feBlend
-                    mode="normal"
-                    in2="BackgroundImageFix"
-                    result="effect1_dropShadow_1_156"
-                  />
-                  <feBlend
-                    mode="normal"
-                    in="SourceGraphic"
-                    in2="effect1_dropShadow_1_156"
-                    result="shape"
-                  />
-                </filter>
-                <linearGradient
-                  id="paint0_linear_1_156"
-                  x1="60.0828"
-                  y1="-3.43391"
-                  x2="119.858"
-                  y2="36.1415"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop offset="0.286458" stop-color="#93EFD9" />
-                  <stop offset="1" stop-color="#ED93EF" />
-                  <stop offset="1" stop-color="#ED93EF" />
-                </linearGradient>
-              </defs>
-            </svg>
-          </a>
-        </div>
+                <feBlend
+                  mode="normal"
+                  in2="BackgroundImageFix"
+                  result="effect1_dropShadow_1_158"
+                />
+                <feBlend
+                  mode="normal"
+                  in="SourceGraphic"
+                  in2="effect1_dropShadow_1_158"
+                  result="shape"
+                />
+              </filter>
+              <linearGradient
+                id="paint0_linear_1_158"
+                x1="66.8994"
+                y1="-3.43391"
+                x2="130.149"
+                y2="46.8165"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop offset="0.286458" stop-color="#93EFD9" />
+                <stop offset="1" stop-color="#ED93EF" />
+                <stop offset="1" stop-color="#ED93EF" />
+              </linearGradient>
+            </defs>
+          </svg>
+        </a>
+        <a
+          href="https://apps.apple.com/app/id1473016754"
+          target="blank"
+          class="hero-download__link hero-download__link--right"
+        >
+          <svg
+            width="110"
+            height="120"
+            viewBox="0 0 110 120"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g filter="url(#filter0_d_1_156)">
+              <path
+                d="M58.506 53.4624L34.5924 27.5237L65.018 46.4181L58.506 53.4624ZM28.3513 26C26.9427 26.797 26 28.2504 26 30.1375V81.8625C26 83.7496 26.9427 85.203 28.3513 86L56.1547 55.9941L28.3513 26ZM74.4231 52.4427L68.0411 48.4458L60.9223 56.0059L68.0411 63.5659L74.5531 59.5691C76.5034 57.8929 76.5034 54.1188 74.4231 52.4427ZM34.5924 84.488L65.018 65.5937L58.506 58.5493L34.5924 84.488Z"
+                fill="url(#paint0_linear_1_156)"
+              />
+            </g>
+            <defs>
+              <filter
+                id="filter0_d_1_156"
+                x="0"
+                y="0"
+                width="110"
+                height="120"
+                filterUnits="userSpaceOnUse"
+                color-interpolation-filters="sRGB"
+              >
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feColorMatrix
+                  in="SourceAlpha"
+                  type="matrix"
+                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                  result="hardAlpha"
+                />
+                <feOffset dx="4" dy="4" />
+                <feGaussianBlur stdDeviation="15" />
+                <feComposite in2="hardAlpha" operator="out" />
+                <feColorMatrix
+                  type="matrix"
+                  values="0 0 0 0 0.741176 0 0 0 0 0 0 0 0 0 1 0 0 0 0.25 0"
+                />
+                <feBlend
+                  mode="normal"
+                  in2="BackgroundImageFix"
+                  result="effect1_dropShadow_1_156"
+                />
+                <feBlend
+                  mode="normal"
+                  in="SourceGraphic"
+                  in2="effect1_dropShadow_1_156"
+                  result="shape"
+                />
+              </filter>
+              <linearGradient
+                id="paint0_linear_1_156"
+                x1="60.0828"
+                y1="-3.43391"
+                x2="119.858"
+                y2="36.1415"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop offset="0.286458" stop-color="#93EFD9" />
+                <stop offset="1" stop-color="#ED93EF" />
+                <stop offset="1" stop-color="#ED93EF" />
+              </linearGradient>
+            </defs>
+          </svg>
+        </a>
+      </div>
     </div>
   </section>
 </template>
