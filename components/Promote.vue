a7810cb362acecb4c90ca1c5c3486cc28f24c046
Sweetysweet <gsm_v@mail.ru>
2022-05-12 19:58:24 +0300

    add promote

--- components/Promote.vue
+++ components/Promote.vue
@@ -2,8 +2,78 @@
   <section class="promote">
     <div class="promote__container">
       <div class="promote-title">
-        <span class="promote-title__text">Opinion Leadder? Promote. </span>
-        <span class="promote-title__text promote-title__text--accent">Earn</span>
+        <span class="promote-title__text">Opinion Leader? Promote. </span>
+        <span class="promote-title__text promote-title__text--accent"
+          >Earn</span
+        >
+      </div>
+      <div class="promote-numbers">
+        <div class="promote-numbers promote-numbers--left">
+          <span class="promote-numbers-price">5,99 $</span>
+          <div class="promote-numbers__description">
+            <span
+              class="
+                promote-numbers__description-title
+              "
+              >Monetization</span
+            >
+            <div
+              class="
+                promote-numbers__description-details
+              "
+            >
+              <span
+                class="
+                  promote-numbers__description-details-light
+                "
+                >Create a
+                <span
+                  class="
+                    promote-numbers__description-details-bold
+                  "
+                  >feed with subscription-only content, the cost of a
+                  subscription is entirely up to you.</span
+                >
+                  Create and publish
+                  promotional posts by selecting the advertising category.
+                </span
+              >
+            </div>
+          </div>
+        </div>
+        <!-- <div class="promote-numbers promote-numbers--right">
+          <span class="promote-numbers-price">+ 31,74 %</span>
+          <div class="promote-numbers__description">
+            <span
+              class="
+                promote-numbers__description-title
+              "
+              >Monetization</span
+            >
+            <div
+              class="
+                promote-numbers__description-details
+              "
+            >
+              <span
+                class="
+                  promote-numbers__description-details-light
+                "
+                >Create a
+                <span
+                  class="
+                    promote-numbers__description-details-bold
+                  "
+                  >feed with subscription-only content, the cost of a
+                  subscription is entirely up to you.</span
+                >
+                  Create and publish
+                  promotional posts by selecting the advertising category.
+                </span
+              >
+            </div>
+          </div>
+        </div> -->
       </div>
     </div>
   </section>
