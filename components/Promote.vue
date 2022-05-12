10ef027521dbfcc17f8f161302c5009836706d7d
sweetySweet <gsm_v@mail.ru>
2022-05-12 23:13:36 +0300

    fix some

--- components/Promote.vue
+++ components/Promote.vue
@@ -41,14 +41,15 @@
             </div>
           </div>
         </div>
-        <!-- <div class="promote-numbers promote-numbers--right">
+        <div class="promote-numbers promote-numbers--right">
           <span class="promote-numbers-price">+ 31,74 %</span>
+          <span class="promote-numbers-procent">CPC</span>
           <div class="promote-numbers__description">
             <span
               class="
                 promote-numbers__description-title
               "
-              >Monetization</span
+              >Fewer touchpoints</span
             >
             <div
               class="
@@ -59,21 +60,19 @@
                 class="
                   promote-numbers__description-details-light
                 "
-                >Create a
+                >When you publish a post,
                 <span
                   class="
                     promote-numbers__description-details-bold
                   "
-                  >feed with subscription-only content, the cost of a
-                  subscription is entirely up to you.</span
+                  >put a price on a digital version of a photo or video for anyone who wants to buy it </span
                 >
-                  Create and publish
-                  promotional posts by selecting the advertising category.
+                  for personal or commercial use with just a couple of clicks in the content card. Also, post with a direct link to the product in the marketplace.
                 </span
               >
             </div>
           </div>
-        </div> -->
+        </div>
       </div>
     </div>
   </section>
