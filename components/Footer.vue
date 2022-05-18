539d1d807abcb0abd7acae020097f676af93345c
Sweetysweet <gsm_v@mail.ru>
2022-05-18 10:43:44 +0300

    footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,21 @@
 <template>
     <footer class="footer">
-        <span>footer</span>
+        <div class="footer__social">
+            <div class="footer__social-email">
+                <span>invest@incosplay.top</span>
+                <span>team@incosplay.top</span>
+            </div>
+            <div class="footer__social-media">
+                <span>Twitter</span>
+                <span>linkedIn</span>
+                <span>AngelList</span>
+            </div>
+             <div class="footer__copyright">
+                <span class="footer__copyright-text">COPYRIGHT © 2022. ALL RIGHTS RESERVED INCOSPLAY INC</span>
+                <span class="footer__copyright-address">Incosplay Inc, 8 The Green, STE A, Dover, DE, USA 19901</span>
+            </div>
+        </div>
+       
     </footer>
 </template>
 <script>
@@ -8,3 +23,6 @@ export default {
     name: 'Footer'
 }
 </script>
+<style lang="scss" >
+@import './Footer.scss';
+</style>
