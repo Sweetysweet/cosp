a8e6b6e2af845dd1df4f1f8e6041198e9ea9f550
Sweetysweet <gsm_v@mail.ru>
2022-05-25 12:22:29 +0300

    fix footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,14 +1,14 @@
 <template>
-    <footer class="footer">
+    <footer class="footer" id="contact">
         <div class="footer__social">
             <div class="footer__social-media">
                 <a class="footer__social-media-links" href="https://twitter.com/incosplay_inc">Twitter</a>
-                <a class="footer__social-media-links" href="https://www.linkedin.com/company/78873033">linkedIn</a>
-                <span>AngelList</span>
+                <a class="footer__social-media-links" href="https://www.linkedin.com/company/78873033">LinkedIn</a>
+                <a class="footer__social-media-links" href="https://angel.co/company/incosplay_inc">AngelList</a>
             </div>
             <div class="footer__policy">
-                <a class="footer__policy-policy" href="https://incosplay.top/privacy_policy/en/">Privacy policy</a>
-                <a class="footer__policy-agreement" href="https://incosplay.top/user_agreement/en/">User agreement</a>
+                <NuxtLink to='/privacy' class="footer__policy-policy">Privacy Policy</NuxtLink>
+                <NuxtLink to='/agreement' class="footer__policy-agreement">User Agreement</NuxtLink>
             </div>
             <div class="footer__copyright">
                 <span class="footer__copyright-text">COPYRIGHT © 2022. INCOSPLAY INC ALL RIGHTS RESERVED</span>
@@ -17,7 +17,6 @@
         </div>
         <div class="fooer__copyright mobile">
             <span class="footer__copyright-text">COPYRIGHT © 2022. INCOSPLAY INC ALL RIGHTS RESERVED</span>
-            <span class="footer__copyright-address mobile-hidden">Incosplay Inc, 8 The Green, STE A, Dover, DE, USA 19901</span>
         </div>
         
        
